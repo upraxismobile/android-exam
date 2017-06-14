e5a22487a5cc2e4d55ceab11b4eeda93351602fb
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-14 18:58:20 +0800

    Moved initialisation of Fabric in BaseApplication

--- Task/app/src/main/java/upraxis/com/task/MainActivity.java
+++ Task/app/src/main/java/upraxis/com/task/MainActivity.java
@@ -1,16 +1,13 @@
 package upraxis.com.task;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
--- Task/app/src/main/java/upraxis/com/task/base/BaseApplication.java
+++ Task/app/src/main/java/upraxis/com/task/base/BaseApplication.java
@@ -2,6 +2,10 @@ package upraxis.com.task.base;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by rsbulanon on 6/14/17.
  */
@@ -11,6 +15,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Fabric.with(this, new Crashlytics());
     }
 }
