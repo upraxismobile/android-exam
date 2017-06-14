0613d2cd5c79072b96ea65bb62fe34220b907e28
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-14 18:23:42 +0800

    Created BaseApplication & BaseActivity
    - Declared BaseApplication in manifest

---
+++ Task/app/src/main/java/upraxis/com/task/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package upraxis.com.task.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rsbulanon on 6/14/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ Task/app/src/main/java/upraxis/com/task/base/BaseApplication.java
@@ -0,0 +1,16 @@
+package upraxis.com.task.base;
+
+import android.app.Application;
+
+/**
+ * Created by rsbulanon on 6/14/17.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
