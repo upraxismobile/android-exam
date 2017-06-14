b7710b744681550f0cccb96dbe807bd2a9d1cf41
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-14 18:39:22 +0800

    Created ContactPerson model

---
+++ Task/app/src/main/java/upraxis/com/task/person/model/ContactPerson.java
@@ -0,0 +1,57 @@
+package upraxis.com.task.person.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by rsbulanon on 6/14/17.
+ */
+
+public class ContactPerson implements Parcelable {
+
+    private String name;
+
+    @SerializedName("mobile_number")
+    private String mobileNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.mobileNumber);
+    }
+
+    public ContactPerson() {
+    }
+
+    protected ContactPerson(Parcel in) {
+        this.name = in.readString();
+        this.mobileNumber = in.readString();
+    }
+
+    public static final Parcelable.Creator<ContactPerson> CREATOR = new Parcelable.Creator<ContactPerson>() {
+        @Override
+        public ContactPerson createFromParcel(Parcel source) {
+            return new ContactPerson(source);
+        }
+
+        @Override
+        public ContactPerson[] newArray(int size) {
+            return new ContactPerson[size];
+        }
+    };
+}
