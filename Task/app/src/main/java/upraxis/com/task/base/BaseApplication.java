cbc9565e511a0ce065d64db027b6dfb877d65829
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-19 00:20:08 +0800

    Added initialisation of AppComponent in BaseApplication

--- Task/app/src/main/java/upraxis/com/task/base/BaseApplication.java
+++ Task/app/src/main/java/upraxis/com/task/base/BaseApplication.java
@@ -5,6 +5,10 @@ import android.app.Application;
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
+import upraxis.com.task.di.component.AppComponent;
+import upraxis.com.task.di.component.DaggerAppComponent;
+import upraxis.com.task.di.module.ApplicationModule;
+import upraxis.com.task.di.module.RetrofitModule;
 
 /**
  * Created by rsbulanon on 6/14/17.
@@ -12,9 +16,28 @@ import io.fabric.sdk.android.Fabric;
 
 public class BaseApplication extends Application {
 
+    private static BaseApplication baseApplication = new BaseApplication();
+    private static AppComponent appComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+
+        getAppComponent();
+    }
+
+    public AppComponent getAppComponent() {
+        if (appComponent == null) {
+            appComponent = DaggerAppComponent.builder()
+                    .applicationModule(new ApplicationModule(this))
+                    .retrofitModule(new RetrofitModule())
+                    .build();
+        }
+        return appComponent;
+    }
+
+    public static BaseApplication getInstance() {
+        return baseApplication;
     }
 }
