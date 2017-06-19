fe485a56a4740fb6e7a308a5c9545655d964628a
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-19 15:35:58 +0800

    Created ErrorMessages class
    - this is to handle static headers and messages for any errors

---
+++ Task/app/src/main/java/upraxis/com/task/commons/ErrorMessages.java
@@ -0,0 +1,18 @@
+package upraxis.com.task.commons;
+
+/**
+ * Created by rsbulanon on 6/19/17.
+ */
+
+public class ErrorMessages {
+
+    /** headers */
+    public static final String ERROR_NO_CONNECTION_HEADER = "Connection Error";
+
+    /** messages */
+    public static final String ERROR_NO_CONNECTION_MESSAGE = "Please check your network connection and try again.";
+    public static final String ERROR_SLOW_CONNECTION = "Unable to complete request due to poor connectivity.";
+    public static final String ERROR_UNAUTHORIZED = "You are not allowed to access this resource.";
+    public static final String ERROR_UNABLE_TO_CONNECT_WITH_SERVER = "Unable to establish communication with server. Please try again.";
+    public static final String ERROR_UNABLE_TO_COMMUNICATE_WITH_SERVER = "You are not allowed to access h";
+}
