63149177d4b2b1247db2a2f1999d338598c0d3ba
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-06-19 23:12:19 +0800

    Added setter/getter methods

--- Task/app/src/main/java/upraxis/com/task/person/model/Person.java
+++ Task/app/src/main/java/upraxis/com/task/person/model/Person.java
@@ -5,17 +5,14 @@ import android.arch.persistence.room.PrimaryKey;
 
 import com.google.gson.annotations.SerializedName;
 
-import lombok.Data;
-
 /**
  * Created by rsbulanon on 6/14/17.
  */
-@Data
 @Entity(tableName = "person")
 public class Person {
 
     @PrimaryKey
-    public String ID;
+    public String id;
 
     @SerializedName("first_name")
     public String firstName;
@@ -33,5 +30,60 @@ public class Person {
     @SerializedName("mobile_number")
     public String mobileNumber;
 
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String ID) {
+        this.id = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
 
